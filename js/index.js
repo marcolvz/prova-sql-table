e413cb7c21c55862a4582bb70f692661731be8b1
Unknown <g.yornad@gmail.com>
2018-06-05 15:04:03 +0200

    Added Ajax

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 $(":button").click(function(){
     var input = $("searchInput").val();
+    
+    $.ajax({
+        url: '',
+        data: input,
+        success: function(response) {
+            
+        },
+        error: function(response) {
+            alert('Si è verificato un errore');
+        }
+    })
 });
